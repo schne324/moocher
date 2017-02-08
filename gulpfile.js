f0c4c646b049513aeba748447dec173871324880
Harris Schneiderman <schne324@gmail.com>
2017-02-08 18:48:41 -0500

    travis stuff

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,12 @@
 
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
+const coveralls = require('gulp-coveralls');
+
+gulp.task('default', ['test']);
 
 gulp.task('test', () => {
   gulp.src('test/index.js', {read: false}).pipe(mocha());
 });
+
+gulp.task('coveralls', () => gulp.src('./coverage/lcov.info').pipe(coveralls()));
